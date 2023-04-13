884d6b17a9b145cc2d05661d132fb5c8c2f180b4
Bora Yarkin <borayarkin02@gmail.com>
2023-04-13 11:53:09 +0300

    Some Game Logic improvements

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
-  if (score >= 1) {
-    if (!guess) {
+  if (score > 1) {
+    if (!guess || guess < 1 || guess > 20) {
       document.querySelector(".message").textContent =
-        "⛔️ No Number Entered!!";
+        "⛔️ Please Enter a number between 1 and 20!!";
     } else if (guess === secretNumber) {
       document.querySelector(".message").textContent = "Correct Number!";
     } else if (guess > secretNumber) {
@@ -33,6 +33,7 @@ document.querySelector(".check").addEventListener("click", function () {
     }
     document.querySelector(".score").textContent = score;
   } else {
-    document.querySelector(".message").textContent = "You lost the  game";
+    document.querySelector(".score").textContent = 0;
+    document.querySelector(".message").textContent = "You lost the game";
   }
 });
