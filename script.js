1d260f8716a5767e6091968354e11a5cbb8e64bc
Bora Yarkin <borayarkin02@gmail.com>
2023-04-13 12:49:04 +0300

    implemented high scores finished the implementation of again button

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-//document.querySelector(".number").textContent = secretNumber;
+//for testing log the random number to the console
 console.log(secretNumber);
+
 let score = 20;
+//when the player wins store the score
+let scores = [];
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+  let guess = Number(document.querySelector(".guess").value);
 
   if (score > 1) {
     //if there is no number or the number is not between 1 and 20
     if (!guess || guess < 1 || guess > 20) {
       document.querySelector(".message").textContent =
-        "⛔️ Please Enter a number between 1 and 20!!";
+        "Please Enter a number between 1 and 20!!";
       //if the player wins
     } else if (guess === secretNumber) {
       document.querySelector(".message").textContent = "Correct Number!";
-
       document.querySelector(".number").textContent = secretNumber;
-
       document.querySelector("body").style.backgroundColor = "#60b347";
-
       document.querySelector(".number").style.width = "30rem";
+
+      //store the score in the scores array and calculate the highest value
+      scores.push(score);
+      let highScore = Math.max(...scores);
+      document.querySelector(".highscore").textContent = highScore;
+
       //if the number is higher than the secret number
     } else if (guess > secretNumber) {
       document.querySelector(".message").textContent = "Too High";
@@ -43,8 +49,14 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
-  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
-  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  //for testing purposes log the updated secret number to the console
+  console.log(secretNumber);
 });
